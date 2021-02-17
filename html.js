cac370f8cded75d920d316c531f3f673863b33be
jarell Boone <jarellboone4@gmail.com>
2021-02-16 23:25:02 -0500

    created test and questions for each

---
+++ html.js
@@ -0,0 +1,92 @@
+
+function makeCard(People) {
+  let newcard =  ""
+  for (let i = 0; i < People.length; i++) {
+      switch (People[i].getRole()) {
+          case "manager":
+              newcard = newcard.concat(`<div class="card" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">${People[i].name}</h5>
+                <h6 class="card-subtitle mb-2 ">${People[i].email}</h6>
+                <h6 class="card-subtitle mb-2 ">${People[i].position}</h6>
+                <h6 class="card-subtitle mb-2 ">${People[i].id}</h6>
+                <h6 class="card-subtitle mb-2 ">${People[i].officenumber}</h6>
+              </div>
+            </div>`)
+              break;
+             
+                case "engineer":
+                    newcard = newcard.concat(`<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">${People[i].name}</h5>
+                      <h6 class="card-subtitle mb-2 ">${People[i].email}</h6>
+                      <h6 class="card-subtitle mb-2 ">${People[i].position}</h6>
+                      <h6 class="card-subtitle mb-2 ">${People[i].id}</h6>
+                      <h6 class="card-subtitle mb-2 ">${People[i].github}</h6>
+                    </div>
+                  </div>`)
+                    break;
+                    
+                case "intern":
+                    newcard = newcard.concat(`<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">${People[i].name}</h5>
+                      <h6 class="card-subtitle mb-2 ">${People[i].email}</h6>
+                      <h6 class="card-subtitle mb-2 ">${People[i].position}</h6>
+                      <h6 class="card-subtitle mb-2 ">${People[i].id}</h6>
+                      <h6 class="card-subtitle mb-2 ">${People[i].school}</h6>
+                    </div>
+                  </div>`)
+                    break;
+         
+      }
+      
+  }
+  return newcard
+}
+
+
+
+
+
+
+
+function generatehtml(People) {
+
+
+
+
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body>
+    <h1> <strong>Team Profile Generator</strong></h1>
+    ${makeCard (People)}
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</html>
+
+
+
+
+
+
+
+
+`
+}
+
+
+
+
+
+module.exports = generatehtml
+
+
+
--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const fs = require("fs")
 const inquirer = require("inquirer")
+const generatehtml = require("./html.js")
 // const Employee = require ("./lib/Employee")
 const Manager = require("./lib/Manager")
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
-
+const People = []
 const questions = [
     {
         type: "input",
         name: "name",
         message: "Whats your name?"
-    } ,
+    },
     {
         type: "list",
         name: "position",
-        message: "Whats your position?" ,
+        message: "Whats your position?",
         choices: ["manager", "engineer", "intern",]
-    } ,
+    },
 
     {
         type: 'input',
         name: 'email',
         message: 'What is their email address?',
-        validate: function (email)
-        {
+        validate: function (email) {
             return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
         }
     },
@@ -31,14 +31,58 @@ const questions = [
         type: "input",
         name: "id",
         message: "Whats your id?"
-    } ,
+    },
     {
         type: "input",
         name: "officenumber",
         message: "Whats your office number?",
         when: (data) => (data.position).includes("manager")
-    } ,
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Whats your github username?",
+        when: (data) => (data.position).includes("engineer")
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "Whats the name of your school?",
+        when: (data) => (data.position).includes("intern")
+    },
+    {
+        type: "confirm",
+        name: "employee",
+        message: "Do you have another employee to add?",
+
+    },
 ]
-inquirer.prompt(questions).then((data) => {
-    console.log(data)
+function init() {
+
+
+    inquirer.prompt(questions).then((data) => {
+        console.log(data)
+        switch (data.position) {
+            case "manager":
+                const resp = new Manager(data.name, data.id, data.email, data.officenumber)
+                People.push(resp)
+                break;
+            case "engineer":
+                const answ = new Engineer(data.name, data.id, data.email, data.github)
+                People.push(answ)
+                break;
+            case "intern":
+                const end = new Intern(data.name, data.id, data.email, data.school)
+                People.push(end)
+                break;
+            
+                
+        }
+        if (data.employee === true) {
+            init()
+        }
+        fs.writeFile("index.html", generatehtml(People), (err) =>
+            err ? console.log("no") : console.log("good job"));
 })
+}
+init()
